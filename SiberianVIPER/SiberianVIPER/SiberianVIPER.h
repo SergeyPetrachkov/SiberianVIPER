fa4cde10513f251178f8e7e2c6a2ac305e50d818
sergey petrachkov <petrachkovsergey@gmail.com>
2017-10-31 10:09:09 +0700

    initial commit with empty project

---
+++ SiberianVIPER/SiberianVIPER/SiberianVIPER.h
@@ -0,0 +1,19 @@
+//
+//  SiberianVIPER.h
+//  SiberianVIPER
+//
+//  Created by Sergey Petrachkov on 31/10/2017.
+//  Copyright © 2017 Sergey Petrachkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SiberianVIPER.
+FOUNDATION_EXPORT double SiberianVIPERVersionNumber;
+
+//! Project version string for SiberianVIPER.
+FOUNDATION_EXPORT const unsigned char SiberianVIPERVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SiberianVIPER/PublicHeader.h>
+
+
