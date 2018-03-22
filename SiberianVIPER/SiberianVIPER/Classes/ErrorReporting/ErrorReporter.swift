bd79c45d245a78da5849a071f76cf0de5537ef79
sergey petrachkov <petrachkovsergey@gmail.com>
2018-03-22 10:38:13 +0700

    add error reporter protocol

---
+++ SiberianVIPER/SiberianVIPER/Classes/ErrorReporting/ErrorReporter.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorReporter.swift
+//  SiberianVIPER
+//
+//  Created by Sergey Petrachkov on 22/03/2018.
+//  Copyright © 2018 Sergey Petrachkov. All rights reserved.
+//
+
+import Foundation
+public protocol ErrorReporter {
+  func track(error: Error)
+}
